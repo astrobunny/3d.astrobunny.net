9153bfa3cda46dbedef0eb33b2e18342758900e6
David Siaw <874280+davidsiaw@users.noreply.github.com>
2024-11-17 11:29:45 +0900

    initial

---
+++ deploy.sh
@@ -0,0 +1,18 @@
+
+rm -rf build
+git clone git@github.com:davidsiaw/nicedoc-template build
+cd build
+git checkout gh-pages
+cd ..
+mv build/.git gitbak
+rm -rf build
+mkdir -p build/weaver
+sleep 2
+docker run --rm -v $PWD/pages:/app/pages -v $PWD/images:/app/images -v $PWD/build:/app/build davidsiaw/nicedoc sh bin/build.sh
+mv gitbak build/.git
+cd build
+echo template.nicedoc.astrobunny.net > CNAME
+git add .
+git commit -m "update"
+git push
+cd ..
