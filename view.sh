1f6a3d23c4d13253c4e4f60e643d994b82f4ab89
David Siaw <874280+davidsiaw@users.noreply.github.com>
2024-11-24 01:09:25 +0900

    image

--- view.sh
+++ view.sh
@@ -1,1 +1,1 @@
-docker run --rm --pull always -v $PWD/pages:/app/pages -v $PWD/images:/app/images -p 4567:4567 davidsiaw/nicedoc bundle exec weaver
+docker run --rm --pull always -v $PWD/pages:/app/pages -v $PWD/images:/app/images -p 4568:4567 davidsiaw/nicedoc bundle exec weaver
